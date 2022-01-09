679749f4fa9d1844ac6e91fddc2c59b4859f2693
piraneshgvs <68136845+piraneshgvs@users.noreply.github.com>
2022-01-09 14:48:36 +0530

    Update HelloWorld.java
    second test

--- HelloWorld.java
+++ HelloWorld.java
@@ -10,10 +10,6 @@ class HelloWorld
 	// Prints "Hello, World" to the terminal window.
 	public static void main(String args[])
 	{
-		Scanner sc=new Scanner(System.in);
-		int a,b;
-		a=sc.nextInt();
-		b=sc.nextInt();
-		System.out.println(a+b);
+		System.out.println("Hai this is piranesh sakthivel");
 	}
 }
