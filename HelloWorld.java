6eec5067c7c7978c55574c90142c3c8740e6a3e5
piraneshgvs <68136845+piraneshgvs@users.noreply.github.com>
2022-01-09 14:42:59 +0530

    Add files via upload
    Jenkin Test file

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+
+// This is a simple Java program.
+// FileName : "HelloWorld.java".
+
+class HelloWorld
+{
+	// Your program begins with a call to main().
+	// Prints "Hello, World" to the terminal window.
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		int a,b;
+		a=sc.nextInt();
+		b=sc.nextInt();
+		System.out.println(a+b);
+	}
+}
